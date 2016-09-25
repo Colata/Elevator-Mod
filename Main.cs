a5fb342b11f3d09a0b398933555409a5a08c86d1
Colata <sammitchell246@gmail.com>
2016-09-26 00:12:34 +1000

    UPDATE

--- Main.cs
+++ Main.cs
@@ -23,14 +23,14 @@ namespace Elevator_Mod
                 badgerBlip.Sprite = BlipSprite.Helicopter;
                 badgerBlip.Scale = 75f;
             }
-        }
 
-        Ped gamePlayer = GTA.Game.Player.Character;
+            UI.Notify("ELEVATOR-MOD Active");
+        }
 
         //Displays the player's status (e.g running, driving, swimming).
         public void current_action()
         {
-             if(gamePlayer.IsAlive)
+            if (gamePlayer.IsAlive)
             {
                 if (gamePlayer.IsInWater)
                 {
@@ -40,16 +40,33 @@ namespace Elevator_Mod
                 {
                     UI.ShowSubtitle("RUNNING");
                 }
-                else if(gamePlayer.IsInVehicle())
+                else if (gamePlayer.IsInVehicle())
                 {
                     UI.ShowSubtitle("DRIVING");
                 }
-             }
+            }
         } 
 
-        bool scriptActive = false;
+        Ped gamePlayer = GTA.Game.Player.Character;
 
-        bool badger_travel = false;
+        public void elevatorZone_UP()
+        {
+            UI.ShowSubtitle("You're in an Elevator zone; Press 'F' to go UP.");
+        }
+
+        public void elevatorZone_DOWN()
+        {
+            UI.ShowSubtitle("You're in an Elevator zone; Press 'F' to go DOWN.");
+        } 
+
+        public void teleportSequence()
+        {
+            Game.FadeScreenIn(750);
+            UI.Notify("Elevator Successful");
+        }
+
+        bool badger_travel_UP = false;
+        bool badger_travel_DOWN = false;
 
         /* METHODS need an access type (Public/Private) and 
            a return type (void/static/ int).*/
@@ -62,13 +79,19 @@ namespace Elevator_Mod
             if (gamePlayer.Position.DistanceTo(badger_BOTTOM) <= 100f)
             {
                 //In-Game Markers
-                Function.Call(Hash.DRAW_MARKER, 2, 478.8547f, -107.6386f, 63.1579f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
-                Function.Call(Hash.DRAW_MARKER, 2, 469.7626f, -107.6571f, 117.6346f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 0.75f, 0.75f, 0.75f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+                Function.Call(Hash.DRAW_MARKER, 1, 478.8547f, -107.6386f, 63.1579f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 1.50f, 1.50f, 1.50f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
+                Function.Call(Hash.DRAW_MARKER, 1, 469.7626f, -107.6571f, 117.6346f, 0.0f, 0.0f, 0.0f, 180.0f, 0.0f, 0.0f, 1.50f, 1.50f, 1.50f, 204, 204, 1, 100, false, true, 2, false, false, false, false);
 
-                if (gamePlayer.Position.DistanceTo(badger_BOTTOM) <= 5f)
+                if (gamePlayer.Position.DistanceTo(badger_BOTTOM) <= 2.5f)
                 {
-                    UI.ShowSubtitle("You're in an Elevator Zone, Press F to travel.");
-                    badger_travel = true;                 
+                    elevatorZone_UP();
+                    badger_travel_UP = true;                 
+                }
+
+                if(gamePlayer.Position.DistanceTo(badger_TOP) <= 2.5f)
+                {
+                    elevatorZone_DOWN();
+                    badger_travel_DOWN = true;
                 }
             }                
         }
@@ -80,18 +103,24 @@ namespace Elevator_Mod
             if (Keys.F4 == e.KeyCode)
             {
                 {
-                    UI.Notify("Key press detected; Script Active");
-                    scriptActive = true;
+                    UI.Notify("Key press detected");
                 }
             }
 
-            if(Keys.F == e.KeyCode && badger_travel)
+            if(Keys.F == e.KeyCode && badger_travel_UP)
             {
-                Game.FadeScreenOut(500);
-                Wait(1000);      
-                Game.FadeScreenIn(500);
+                Game.FadeScreenOut(750);
+                Wait(750);
                 gamePlayer.Position = badger_TOP;
-                UI.Notify("Teleport Successful");
+                teleportSequence();             
+            }
+
+            if(Keys.F == e.KeyCode && badger_travel_DOWN)
+            {
+                Game.FadeScreenOut(750);
+                Wait(750);
+                gamePlayer.Position = badger_BOTTOM;
+                teleportSequence();
             }
         }
     }
